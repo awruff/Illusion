b6bfd82ff852a5078fc08398d2b838044b5aa4ba
Syz66 <sillysys+syz@proton.me>
2025-04-16 23:07:52 +0000

    events !!

--- src/main/java/org/example/illusion/mixins/events/MixinMinecraft_KeyPressEvent.java
+++ src/main/java/org/example/illusion/mixins/events/MixinMinecraft_KeyPressEvent.java
@@ -13,12 +13,6 @@ public class MixinMinecraft_KeyPressEvent {
 
     @Inject(method = "runTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Minecraft;dispatchKeypresses()V"))
     private void illusion$publishKeyPress(CallbackInfo ci) {
-//        for (Module module : Illusion.INSTANCE.getModuleManager().getModules().values()) {
-//            if (Keyboard.isKeyDown(module.getBind())) {
-//                module.toggle();
-//            }
-//        }
-
         Illusion.INSTANCE.getEventBus().publish(new KeyPressEvent());
     }
 
