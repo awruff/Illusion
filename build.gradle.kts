f89021fecbff6e7b952f6de06a36986194da9697
syz66 <sillysys+syz@proton.me>
2025-04-22 21:58:26 -0400

    gradle stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,22 +4,22 @@ import dev.deftu.gradle.utils.GameSide
 
 plugins {
     id("java")
-    id("dev.deftu.gradle.tools") version("2.30.0")
-    id("dev.deftu.gradle.tools.resources") version("2.30.0")
-    id("dev.deftu.gradle.tools.bloom") version("2.30.0")
-    id("dev.deftu.gradle.tools.shadow") version("2.30.0")
-    id("dev.deftu.gradle.tools.minecraft.loom") version("2.30.0")
+    alias(libs.plugins.dgt)
+    alias(libs.plugins.dgt.resources)
+    alias(libs.plugins.dgt.bloom)
+    alias(libs.plugins.dgt.shadow)
+    alias(libs.plugins.dgt.loom)
 }
 
 toolkitLoomHelper {
     // mixins
-    useMixinExtras("0.4.1")
+    useMixinExtras("0.4.+")
     useMixinRefMap(modData.id)
     useForgeMixin(modData.id)
     useTweaker("org.spongepowered.asm.launch.MixinTweaker")
 
     // run configs
-    useDevAuth("1.2.1")
+    useDevAuth("1.2.+")
     useProperty("mixin.debug.export", "true", GameSide.CLIENT)
     disableRunConfigs(GameSide.SERVER)
 }
@@ -30,11 +30,9 @@ repositories {
 }
 
 dependencies {
-    implementation(shade("org.polyfrost:polymixin:0.8.4+build.2") {
-        isTransitive = false
-    })
+    implementation(libs.polymixin)
+    shade(libs.polymixin)
 
-    implementation(shade("io.github.nevalackin:radbus:1.0.0") {
-        isTransitive = false
-    })
+    implementation(libs.radbus)
+    shade(libs.radbus)
 }
