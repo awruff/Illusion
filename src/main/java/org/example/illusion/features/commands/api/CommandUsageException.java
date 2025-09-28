77b701d06fe690d03778411139f5af395533ed22
awruff <sillysys@Proton.me>
2025-09-28 08:20:57 -0400

    Remove: Commands

--- src/main/java/org/example/illusion/features/commands/api/CommandUsageException.java
+++
@@ -1,7 +0,0 @@
-package org.example.illusion.features.commands.api;
-
-public class CommandUsageException extends Exception {
-    public CommandUsageException(String message) {
-        super(message);
-    }
-}
