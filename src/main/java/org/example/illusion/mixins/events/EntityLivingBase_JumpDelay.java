e60033ee8407ea08a8c90774f4d3bf17e1e3ca93
awruff <sillysys@Proton.me>
2025-09-28 08:21:48 -0400

    Fix: JumpDelay Event

--- src/main/java/org/example/illusion/mixins/events/EntityLivingBase_JumpDelay.java
+++ src/main/java/org/example/illusion/mixins/events/EntityLivingBase_JumpDelay.java
@@ -1,29 +1,21 @@
 package org.example.illusion.mixins.events;
 
-import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.EntityLivingBase;
 import org.example.illusion.IllusionClient;
 import org.example.illusion.event.impl.player.JumpDelayEvent;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(EntityLivingBase.class)
 public class EntityLivingBase_JumpDelay {
-    @Redirect(
+    @ModifyConstant(
             method = "onLivingUpdate",
-            at = @At(
-                    value = "FIELD",
-                    target = "Lnet/minecraft/entity/EntityLivingBase;jumpTicks:I",
-                    opcode = Opcodes.PUTFIELD
-            )
+            constant = @Constant(intValue = 10)
     )
-    private void illusion$publishJumpDelay(EntityLivingBase instance, int value) {
-        if (instance instanceof EntityPlayerSP) {
-            JumpDelayEvent event = new JumpDelayEvent();
-            IllusionClient.getInstance().getEventBus().publish(event);
-            value = event.getJumpTicks();
-        }
+    private int illusion$publishJumpDelay(int constant) {
+        JumpDelayEvent event = new JumpDelayEvent();
+        IllusionClient.getInstance().getEventBus().publish(event);
+        return event.getJumpTicks();
     }
 }
